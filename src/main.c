d46b321a54dd8462a19168b3a4679ee6359b36dc
George Watson <gigolo@hotmail.co.uk>
2024-06-07 19:51:43 +0100

    removed fbo

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "sokol_helpers/sokol_input.h"
 #include "sokol_helpers/sokol_img.h"
 #include "sokol_helpers/sokol_scalefactor.h"
+#include "sokol_helpers/sokol_generic.h"
 #define SOKOL_IMPL
 #include "sokol_gfx.h"
 #include "sokol_gp.h"
@@ -19,16 +20,32 @@ typedef struct {
     int width, height;
 } Texture;
 
+static uint8_t fake_bitmask(int t, int tr, int r, int br, int b, int bl, int l, int tl) {
+    if (!t && !l) tl = 0;
+    if (!t && !r) tr = 0;
+    if (!b && !l) bl = 0;
+    if (!b && !r) br = 0;
+    uint8_t result = 0;
+    if (tl) result += (1 << 0);
+    if (t)  result += (1 << 1);
+    if (tr) result += (1 << 2);
+    if (l)  result += (1 << 3);
+    if (r)  result += (1 << 4);
+    if (bl) result += (1 << 5);
+    if (b)  result += (1 << 6);
+    if (br) result += (1 << 7);
+    return result;
+}
+
+typedef bool TileBitmask[9];
+
 static struct {
     struct {
-        struct {
-            sg_image color;
-            sg_image depth;
-            sg_attachments attachements;
-            sg_sampler sampler;
-        } framebuffer;
+        TileBitmask *grid;
         float scale;
         bool open;
+        int spriteW, spriteX;
+        int spriteH, spriteY;
         Texture default_2x2;
         Texture default_3x3;
         Texture *currentAtlas;
@@ -36,35 +53,33 @@ static struct {
     Map map;
 } state;
 
-static void LoadTexture(Texture *atlas, const char *path) {
-    atlas->texture = sg_load_texture_path_ex(path, &atlas->width, &atlas->height);
-}
-
-static void draw_fbo(void) {
-    float w = state.mask.currentAtlas->width;
-    float h = state.mask.currentAtlas->height;
-    sgp_begin(w, h);
-    sgp_project(0, w, 0, h);
-    sgp_rect dst = { 0, 0, w, h };
-    sgp_rect src = { 0, 0, state.mask.currentAtlas->width, state.mask.currentAtlas->height };
-    sgp_set_image(0, state.mask.currentAtlas->texture);
-    sgp_draw_textured_rect(0, dst, src);
-    sgp_reset_image(0);
+static void InitMaskEditor(void) {
+    state.mask.default_2x2.texture = sg_load_texture_path_ex("/Users/george/git/tyler/assets/default_2x2.png", &state.mask.default_2x2.width, &state.mask.default_2x2.height);
+    state.mask.default_3x3.texture = sg_load_texture_path_ex("/Users/george/git/tyler/assets/default_3x3.png", &state.mask.default_3x3.width, &state.mask.default_3x3.height);
+    state.mask.currentAtlas = &state.mask.default_3x3;
     
-    sg_pass_action pass_action = {0};
-    pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
-    pass_action.colors[0].clear_value = (sg_color){.2f,.2f,.2f,.2f};
-    sg_pass pass = {
-        .action = pass_action,
-        .attachments = state.mask.framebuffer.attachements
-    };
-    sg_begin_pass(&pass);
-    sgp_flush();
-    sgp_end();
-    sg_end_pass();
+    state.mask.open = true;
+    state.mask.scale = 4.f;
+    
+    state.mask.spriteW = state.mask.spriteH = 8;
+    state.mask.spriteX = state.mask.default_3x3.width / state.mask.spriteW;
+    state.mask.spriteY = state.mask.default_3x3.height / state.mask.spriteH;
+    size_t sz = state.mask.spriteX * state.mask.spriteY * sizeof(TileBitmask);
+    state.mask.grid = malloc(sz);
+    memset(state.mask.grid, 0, sz);
+    for (int x = 0; x < state.mask.spriteX; x++)
+        for (int y = 0; y < state.mask.spriteY; y++)
+            state.mask.grid[y * 3 + x][4] = true;
 }
 
-static void testcb(const ImDrawList* dl, const ImDrawCmd* cmd) {
+static void DestroyMaskEditor(void) {
+    sokol_helper_destroy(state.mask.default_2x2.texture);
+    sokol_helper_destroy(state.mask.default_3x3.texture);
+    if (state.mask.grid)
+        free(state.mask.grid);
+}
+
+static void igDrawMaskEditorCb(const ImDrawList* dl, const ImDrawCmd* cmd) {
     int sw = state.mask.currentAtlas->width*state.mask.scale;
     int sh = state.mask.currentAtlas->height*state.mask.scale;
     int cx = (int) cmd->ClipRect.x;
@@ -72,12 +87,16 @@ static void testcb(const ImDrawList* dl, const ImDrawCmd* cmd) {
     int cw = (int) (cmd->ClipRect.z - cmd->ClipRect.x);
     int ch = (int) (cmd->ClipRect.w - cmd->ClipRect.y);
     sgp_scissor(cx, cy, cw, ch);
-    sgp_viewport(cx ? cx : cw - sw, cy ? cy : ch - sh, sw, sh);
-    sgp_set_image(0, state.mask.framebuffer.color);
-    sgp_set_sampler(0, state.mask.framebuffer.sampler);
+    cx = cx ? cx : cw - sw;
+    cy = cy ? cy : ch - sh;
+    sgp_viewport(cx, cy, sw, sh);
+    sgp_set_image(0, state.mask.currentAtlas->texture);
     sgp_push_transform();
     sgp_scale(state.mask.scale, state.mask.scale);
     sgp_draw_filled_rect(0, 0, state.mask.currentAtlas->width, state.mask.currentAtlas->height);
+    sgp_set_color(1.f, 0.f, 0.f, 1.f);
+    sgp_set_blend_mode(SGP_BLENDMODE_NONE);
+    sgp_reset_color();
     sgp_pop_transform();
     sgp_reset_image(0);
     sgp_reset_sampler(0);
@@ -85,19 +104,15 @@ static void testcb(const ImDrawList* dl, const ImDrawCmd* cmd) {
     sgp_end();
 }
 
-// Called on every frame of the application.
 static void frame(void) {
     int width = sapp_width(), height = sapp_height();
     sgp_begin(width, height);
     
-    // draw background
     sgp_set_color(0.05f, 0.05f, 0.05f, 1.0f);
     sgp_clear();
     sgp_reset_color();
-    
     sgp_set_blend_mode(SGP_BLENDMODE_BLEND);
-    draw_fbo();
-
+    
     simgui_frame_desc_t igFrameDesc = {
         .width = width,
         .height = height,
@@ -108,21 +123,19 @@ static void frame(void) {
     
     igSetNextWindowPos((ImVec2){20, 20}, ImGuiCond_Once, (ImVec2){0,0});
     igSetNextWindowSize((ImVec2){800, 400}, ImGuiCond_Once);
-    if (igBegin("Mask Editor", 0, 0)) {
+    if (state.mask.open && igBegin("Mask Editor", &state.mask.open, ImGuiWindowFlags_AlwaysAutoResize)) {
         if (igBeginChild_Str("Mask Editor", (ImVec2){state.mask.currentAtlas->width*state.mask.scale, state.mask.currentAtlas->height*state.mask.scale}, true, ImGuiWindowFlags_None)) {
             ImDrawList* dl = igGetWindowDrawList();
-            ImDrawList_AddCallback(dl, testcb, NULL);
+            ImDrawList_AddCallback(dl, igDrawMaskEditorCb, NULL);
         }
         igEndChild();
+        igEnd();
     }
-    igEnd();
     
 
     sgp_viewport(0, 0, width, height);
     float ratio = width/(float)height;
-       // Set drawing coordinate space to (left=-ratio, right=ratio, top=1, bottom=-1).
-       sgp_project(-ratio, ratio, 1.0f, -1.0f);
-    // Draw an animated rectangle that rotates and changes its colors.
+    sgp_project(-ratio, ratio, 1.0f, -1.0f);
     float time = sapp_frame_count() * sapp_frame_duration();
     float r = sinf(time)*0.5+0.5, g = cosf(time)*0.5+0.5;
     sgp_push_transform();
@@ -132,7 +145,6 @@ static void frame(void) {
     sgp_pop_transform();
     sgp_reset_color();
 
-    // dispatch draw commands
     sg_pass pass = {.swapchain = sglue_swapchain()};
     sg_begin_pass(&pass);
     sgp_flush();
@@ -142,9 +154,7 @@ static void frame(void) {
     sg_commit();
 }
 
-// Called when the application is initializing.
 static void init(void) {
-    // Initialize Sokol GFX.
     sg_desc sgdesc = {
         .environment = sglue_environment(),
         .logger.func = slog_func
@@ -155,7 +165,6 @@ static void init(void) {
         exit(-1);
     }
 
-    // Initialize Sokol GP, adjust the size of command buffers for your own use.
     sgp_desc sgpdesc = {0};
     sgp_setup(&sgpdesc);
     if (!sgp_is_valid()) {
@@ -170,57 +179,7 @@ static void init(void) {
     
     stm_setup();
     
-    // create frame buffer image
-    sg_image_desc fb_image_desc = {0};
-    fb_image_desc.render_target = true;
-    fb_image_desc.width = 128;
-    fb_image_desc.height = 128;
-    state.mask.framebuffer.color = sg_make_image(&fb_image_desc);
-    if (sg_query_image_state(state.mask.framebuffer.color) != SG_RESOURCESTATE_VALID) {
-        fprintf(stderr, "Failed to create frame buffer image\n");
-        exit(-1);
-    }
-    
-    // create frame buffer depth stencil
-    sg_image_desc fb_depth_image_desc = {0};
-    fb_depth_image_desc.render_target = true;
-    fb_depth_image_desc.width = 128;
-    fb_depth_image_desc.height = 128;
-    fb_depth_image_desc.pixel_format = sapp_depth_format();
-    state.mask.framebuffer.depth = sg_make_image(&fb_depth_image_desc);
-    if (sg_query_image_state(state.mask.framebuffer.depth) != SG_RESOURCESTATE_VALID) {
-        fprintf(stderr, "Failed to create frame buffer depth image\n");
-        exit(-1);
-    }
-    
-    // create frame buffer attachments
-    sg_attachments_desc fb_attachments_desc = {
-        .colors = {{.image=state.mask.framebuffer.color}},
-        .depth_stencil = {.image=state.mask.framebuffer.depth}
-    };
-    state.mask.framebuffer.attachements = sg_make_attachments(&fb_attachments_desc);
-    if (sg_query_attachments_state(state.mask.framebuffer.attachements) != SG_RESOURCESTATE_VALID) {
-        fprintf(stderr, "Failed to create frame buffer attachments\n");
-        exit(-1);
-    }
-    
-    // create linear sampler
-    sg_sampler_desc linear_sampler_desc = {
-        .min_filter = SG_FILTER_NEAREST,
-        .mag_filter = SG_FILTER_NEAREST,
-        .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
-        .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
-    };
-    state.mask.framebuffer.sampler = sg_make_sampler(&linear_sampler_desc);
-    if (sg_query_sampler_state(state.mask.framebuffer.sampler) != SG_RESOURCESTATE_VALID) {
-        fprintf(stderr, "failed to create linear sampler");
-        exit(-1);
-    }
-    
-    LoadTexture(&state.mask.default_2x2, "/Users/george/git/tyler/assets/default_2x2.png");
-    LoadTexture(&state.mask.default_3x3, "/Users/george/git/tyler/assets/default_3x3.png");
-    state.mask.currentAtlas = &state.mask.default_3x3;
-    state.mask.scale = 4.f;
+    InitMaskEditor();
     InitMap(&state.map, 64, 64);
 }
 
@@ -229,11 +188,11 @@ static void input(const sapp_event *e) {
     sokol_input_handler(e);
 }
 
-// Called when the application is shutting down.
 static void cleanup(void) {
     DestroyMap(&state.map);
-    // Cleanup Sokol GP and Sokol GFX resources.
+    DestroyMaskEditor();
     sgp_shutdown();
+    simgui_shutdown();
     sg_shutdown();
 }
 
