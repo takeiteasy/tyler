4aee7cc47b847736918c0c816da9b4b29caff957
George Watson <gigolo@hotmail.co.uk>
2024-06-07 15:53:20 +0100

    working on mask editor

--- src/main.c
+++ src/main.c
@@ -14,59 +14,49 @@
 #include "cimgui/cimgui.h"
 #include "sokol_imgui.h"
 
-static struct {
-    struct {
-        sg_image color;
-        sg_image depth;
-        sg_attachments attachements;
-        sg_sampler sampler;
-    } framebuffer;
-    bool open;
-} MaskEditor;
+typedef struct {
+    sg_image texture;
+    int width, height;
+} Texture;
 
 static struct {
-    sg_image default_2x2;
-    sg_image default_3x3;
+    struct {
+        struct {
+            sg_image color;
+            sg_image depth;
+            sg_attachments attachements;
+            sg_sampler sampler;
+        } framebuffer;
+        float scale;
+        bool open;
+        Texture default_2x2;
+        Texture default_3x3;
+        Texture *currentAtlas;
+    } mask;
     Map map;
 } state;
 
-static void draw_triangles(void) {
-    const float PI = 3.14159265f;
-    static sgp_vec2 points_buffer[4096];
-
-    sgp_irect viewport = sgp_query_state()->viewport;
-    int width = viewport.w, height = viewport.h;
-    float hw = width * 0.5f;
-    float hh = height * 0.5f;
-    float w = height*0.3f;
-    unsigned int count = 0;
-    float step = (2.0f*PI)/6.0f;
-    for (float theta = 0.0f; theta <= 2.0f*PI + step*0.5f; theta+=step) {
-        sgp_vec2 v = {hw*1.33f + w*cosf(theta), hh*1.33f - w*sinf(theta)};
-        points_buffer[count++] = v;
-        if (count % 3 == 1) {
-            sgp_vec2 u = {hw, hh};
-            points_buffer[count++] = u;
-        }
-    }
-    sgp_set_color(1.0f, 0.0f, 1.0f, 1.0f);
-    sgp_draw_filled_triangles_strip(points_buffer, count);
+static void LoadTexture(Texture *atlas, const char *path) {
+    atlas->texture = sg_load_texture_path_ex(path, &atlas->width, &atlas->height);
 }
 
 static void draw_fbo(void) {
-    sgp_begin(128, 128);
-    sgp_project(0, 128, 128, 0);
-    draw_triangles();
-
+    float w = state.mask.currentAtlas->width;
+    float h = state.mask.currentAtlas->height;
+    sgp_begin(w, h);
+    sgp_project(0, w, 0, h);
+    sgp_rect dst = { 0, 0, w, h };
+    sgp_rect src = { 0, 0, state.mask.currentAtlas->width, state.mask.currentAtlas->height };
+    sgp_set_image(0, state.mask.currentAtlas->texture);
+    sgp_draw_textured_rect(0, dst, src);
+    sgp_reset_image(0);
+    
     sg_pass_action pass_action = {0};
     pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
-    pass_action.colors[0].clear_value.r = 1.0f;
-    pass_action.colors[0].clear_value.g = 1.0f;
-    pass_action.colors[0].clear_value.b = 1.0f;
-    pass_action.colors[0].clear_value.a = 0.2f;
+    pass_action.colors[0].clear_value = (sg_color){.2f,.2f,.2f,.2f};
     sg_pass pass = {
         .action = pass_action,
-        .attachments = MaskEditor.framebuffer.attachements
+        .attachments = state.mask.framebuffer.attachements
     };
     sg_begin_pass(&pass);
     sgp_flush();
@@ -75,15 +65,20 @@ static void draw_fbo(void) {
 }
 
 static void testcb(const ImDrawList* dl, const ImDrawCmd* cmd) {
-    const int cx = (int) cmd->ClipRect.x;
-    const int cy = (int) cmd->ClipRect.y;
-    const int cw = (int) (cmd->ClipRect.z - cmd->ClipRect.x);
-    const int ch = (int) (cmd->ClipRect.w - cmd->ClipRect.y);
+    int sw = state.mask.currentAtlas->width*state.mask.scale;
+    int sh = state.mask.currentAtlas->height*state.mask.scale;
+    int cx = (int) cmd->ClipRect.x;
+    int cy = (int) cmd->ClipRect.y;
+    int cw = (int) (cmd->ClipRect.z - cmd->ClipRect.x);
+    int ch = (int) (cmd->ClipRect.w - cmd->ClipRect.y);
     sgp_scissor(cx, cy, cw, ch);
-    sgp_viewport(cx, cy, 360, 360);
-    sgp_set_image(0, MaskEditor.framebuffer.color);
-    sgp_set_sampler(0, MaskEditor.framebuffer.sampler);
-    sgp_draw_filled_rect(0, 0, 128, 128);
+    sgp_viewport(cx ? cx : cw - sw, cy ? cy : ch - sh, sw, sh);
+    sgp_set_image(0, state.mask.framebuffer.color);
+    sgp_set_sampler(0, state.mask.framebuffer.sampler);
+    sgp_push_transform();
+    sgp_scale(state.mask.scale, state.mask.scale);
+    sgp_draw_filled_rect(0, 0, state.mask.currentAtlas->width, state.mask.currentAtlas->height);
+    sgp_pop_transform();
     sgp_reset_image(0);
     sgp_reset_sampler(0);
     sgp_flush();
@@ -100,7 +95,6 @@ static void frame(void) {
     sgp_clear();
     sgp_reset_color();
     
-    
     sgp_set_blend_mode(SGP_BLENDMODE_BLEND);
     draw_fbo();
 
@@ -114,8 +108,8 @@ static void frame(void) {
     
     igSetNextWindowPos((ImVec2){20, 20}, ImGuiCond_Once, (ImVec2){0,0});
     igSetNextWindowSize((ImVec2){800, 400}, ImGuiCond_Once);
-    if (igBegin("Dear ImGui", 0, 0)) {
-        if (igBeginChild_Str("sokol-gfx", (ImVec2){360, 360}, true, ImGuiWindowFlags_None)) {
+    if (igBegin("Mask Editor", 0, 0)) {
+        if (igBeginChild_Str("Mask Editor", (ImVec2){state.mask.currentAtlas->width*state.mask.scale, state.mask.currentAtlas->height*state.mask.scale}, true, ImGuiWindowFlags_None)) {
             ImDrawList* dl = igGetWindowDrawList();
             ImDrawList_AddCallback(dl, testcb, NULL);
         }
@@ -181,8 +175,8 @@ static void init(void) {
     fb_image_desc.render_target = true;
     fb_image_desc.width = 128;
     fb_image_desc.height = 128;
-    MaskEditor.framebuffer.color = sg_make_image(&fb_image_desc);
-    if (sg_query_image_state(MaskEditor.framebuffer.color) != SG_RESOURCESTATE_VALID) {
+    state.mask.framebuffer.color = sg_make_image(&fb_image_desc);
+    if (sg_query_image_state(state.mask.framebuffer.color) != SG_RESOURCESTATE_VALID) {
         fprintf(stderr, "Failed to create frame buffer image\n");
         exit(-1);
     }
@@ -193,38 +187,40 @@ static void init(void) {
     fb_depth_image_desc.width = 128;
     fb_depth_image_desc.height = 128;
     fb_depth_image_desc.pixel_format = sapp_depth_format();
-    MaskEditor.framebuffer.depth = sg_make_image(&fb_depth_image_desc);
-    if (sg_query_image_state(MaskEditor.framebuffer.depth) != SG_RESOURCESTATE_VALID) {
+    state.mask.framebuffer.depth = sg_make_image(&fb_depth_image_desc);
+    if (sg_query_image_state(state.mask.framebuffer.depth) != SG_RESOURCESTATE_VALID) {
         fprintf(stderr, "Failed to create frame buffer depth image\n");
         exit(-1);
     }
     
     // create frame buffer attachments
     sg_attachments_desc fb_attachments_desc = {
-        .colors = {{.image=MaskEditor.framebuffer.color}},
-        .depth_stencil = {.image=MaskEditor.framebuffer.depth}
+        .colors = {{.image=state.mask.framebuffer.color}},
+        .depth_stencil = {.image=state.mask.framebuffer.depth}
     };
-    MaskEditor.framebuffer.attachements = sg_make_attachments(&fb_attachments_desc);
-    if (sg_query_attachments_state(MaskEditor.framebuffer.attachements) != SG_RESOURCESTATE_VALID) {
+    state.mask.framebuffer.attachements = sg_make_attachments(&fb_attachments_desc);
+    if (sg_query_attachments_state(state.mask.framebuffer.attachements) != SG_RESOURCESTATE_VALID) {
         fprintf(stderr, "Failed to create frame buffer attachments\n");
         exit(-1);
     }
     
     // create linear sampler
     sg_sampler_desc linear_sampler_desc = {
-        .min_filter = SG_FILTER_LINEAR,
-        .mag_filter = SG_FILTER_LINEAR,
+        .min_filter = SG_FILTER_NEAREST,
+        .mag_filter = SG_FILTER_NEAREST,
         .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
         .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
     };
-    MaskEditor.framebuffer.sampler = sg_make_sampler(&linear_sampler_desc);
-    if (sg_query_sampler_state(MaskEditor.framebuffer.sampler) != SG_RESOURCESTATE_VALID) {
+    state.mask.framebuffer.sampler = sg_make_sampler(&linear_sampler_desc);
+    if (sg_query_sampler_state(state.mask.framebuffer.sampler) != SG_RESOURCESTATE_VALID) {
         fprintf(stderr, "failed to create linear sampler");
         exit(-1);
     }
     
-    state.default_2x2 = sg_load_texture_path("/Users/george/git/tyler/assets/default_2x2.png");
-    state.default_3x3 = sg_load_texture_path("/Users/george/git/tyler/assets/default_3x3.png");
+    LoadTexture(&state.mask.default_2x2, "/Users/george/git/tyler/assets/default_2x2.png");
+    LoadTexture(&state.mask.default_3x3, "/Users/george/git/tyler/assets/default_3x3.png");
+    state.mask.currentAtlas = &state.mask.default_3x3;
+    state.mask.scale = 4.f;
     InitMap(&state.map, 64, 64);
 }
 
